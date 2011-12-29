9f712b7f5f77a00849d777e5db260d4f637d0453
OlegPasko <olegpasko@gmail.com>
2011-12-29 16:15:44 +0200

    Initial Commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 # -*- encoding : utf-8 -*-
 class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
@@ -12,4 +17,5 @@ class UsersController < ApplicationController
       render "new"
     end
   end
+  
 end
