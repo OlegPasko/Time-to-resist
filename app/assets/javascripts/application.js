b8f40affea50cadac3ff1b0886d9b6f4959ef541
OlegPasko <olegpasko@gmail.com>
2011-12-29 23:01:32 +0200

    table

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+$(document).ready(function() {
+    $('#users').dataTable( {
+        "sPaginationType": "full_numbers",
+        "aaSorting": [[ 0, "desc" ]]
+    } );
+} );
