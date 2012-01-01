7ed4ae8474c507d275a47152d16b0413c083f024
OlegPasko <olegpasko@gmail.com>
2012-01-01 18:12:46 +0200

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,12 @@ class User < ActiveRecord::Base
   validates_presence_of :maxhealth
   validates_presence_of :craft
   validates_uniqueness_of :email
-  validates_inclusion_of :level, :in => 1..120
-  validates_inclusion_of :maxhealth, :in => 1..20000
-  validates_inclusion_of :craft, :in => 0..650
+  validates_inclusion_of :level, :in => 1..120,
+             :message => "Nononono"
+  validates_inclusion_of :maxhealth, :in => 1..20000,
+             :message => "Nononono"
+  validates_inclusion_of :craft, :in => 0..650,
+             :message => "Nononono"
   
   has_one    :mission
   has_one    :character
