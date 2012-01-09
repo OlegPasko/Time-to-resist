3a3a280ce6ed9d9522795481efb6b455f5a7a275
OlegPasko <olegpasko@gmail.com>
2012-01-09 13:49:13 +0200

    walls upd

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,8 @@ class PagesController < ApplicationController
     @lastusers = User.find(:all, :order => "created_at DESC", :limit => 14)
     @lastrewards = Reward.find(:all, :order => "created_at DESC", :limit => 7)
     @walls = Wall.limit(15)
+    
+    @newwall = Wall.new
   end
   
   def gallery
