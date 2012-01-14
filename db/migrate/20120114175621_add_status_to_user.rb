12ed8631ca502461a6710ac0b4a5acc40213494e
OlegPasko <olegpasko@gmail.com>
2012-01-14 20:28:53 +0200

    fix

--- db/migrate/20120114175621_add_status_to_user.rb
+++ db/migrate/20120114175621_add_status_to_user.rb
@@ -1,5 +1,5 @@
 class AddStatusToUser < ActiveRecord::Migration
   def change
-    add_column :users, :status, :boolean, :default => 1
+    add_column :users, :status, :boolean, :default => true
   end
 end
